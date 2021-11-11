37920ee56978e7a47cd08b52c93d4389edf7e559
Shaun Lawrence <shaunrlawrence@gmail.com>
2021-11-11 22:02:21 +0000

    Part four complete
    Added ViewModel related bits

---
+++ Pairs/Pairs/ViewModels/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+public class BaseViewModel : INotifyPropertyChanged
+{
+    protected virtual bool SetProperty<T>(
+        ref T backingStore,
+        T value,
+        [CallerMemberName] string propertyName = "")
+    {
+        if (EqualityComparer<T>.Default.Equals(backingStore, value))
+        {
+            return false;
+        }
+
+        backingStore = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
---
+++ Pairs/Pairs/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Pairs.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        private int guessedCount;
+        private bool showGuess;
+        private bool showPlay = true;
+
+        public int GuessedCount
+        {
+            get => guessedCount;
+            set => SetProperty(ref guessedCount, value);
+        }
+
+        public bool ShowGuess
+        {
+            get => showGuess;
+            set => SetProperty(ref showGuess, value);
+        }
+
+        public bool ShowPlay
+        {
+            get => showPlay;
+            set => SetProperty(ref showPlay, value);
+        }
+
+        public ICommand GuessCommand { get; }
+
+        public ICommand PlayCommand { get; }
+
+        public MainPageViewModel()
+        {
+            GuessCommand = new Command(() => OnGuess());
+            PlayCommand = new Command(() => OnPlay());
+        }
+
+        private void OnGuess()
+        {
+            GuessedCount++;
+        }
+
+        private void OnPlay()
+        {
+            ShowPlay = false;
+            ShowGuess = true;
+        }
+    }
+}
